ae4907d2c4b48ff98f8a5091963f6c8d27e22ebe
Erik <erik@montnemery.com>
2022-05-30 09:03:22 +0200

    Improve handling of MQTT config entry data

--- homeassistant/components/mqtt/__init__.py
+++ homeassistant/components/mqtt/__init__.py
@@ -102,6 +102,16 @@ CONNECTION_SUCCESS = "connection_success"
 CONNECTION_FAILED = "connection_failed"
 CONNECTION_FAILED_RECOVERABLE = "connection_failed_recoverable"
 
+CONFIG_ENTRY_CONFIG_KEYS = [
+    CONF_BIRTH_MESSAGE,
+    CONF_BROKER,
+    CONF_DISCOVERY,
+    CONF_PASSWORD,
+    CONF_PORT,
+    CONF_USERNAME,
+    CONF_WILL_MESSAGE,
+]
+
 CONFIG_SCHEMA = vol.Schema(
     {
         DOMAIN: vol.All(
@@ -185,6 +195,18 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     return True
 
 
+def _filter_entry_config(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """Remove unknown keys from config entry data.
+
+    Extra keys may have been added when importing MQTT yaml configuration.
+    """
+    filtered_data = {
+        k: entry.data[k] for k in CONFIG_ENTRY_CONFIG_KEYS if k in entry.data
+    }
+    if entry.data.keys() != filtered_data.keys():
+        hass.config_entries.async_update_entry(entry, data=filtered_data)
+
+
 def _merge_basic_config(
     hass: HomeAssistant, entry: ConfigEntry, yaml_config: dict[str, Any]
 ) -> None:
@@ -238,6 +260,9 @@ async def _async_config_entry_updated(hass: HomeAssistant, entry: ConfigEntry) -
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Load a config entry."""
+    # Remove unknown keys
+    _filter_entry_config(hass, entry)
+
     # Merge basic configuration, and add missing defaults for basic options
     _merge_basic_config(hass, entry, hass.data.get(DATA_MQTT_CONFIG, {}))
 
